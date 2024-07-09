7c13edefe2f5d09c04f080dd70f0e3c6baeecf8e
Heiko Kast <heiko.kast@mercedes-benz.com>
2024-07-09 10:08:44 +0200

    feat: allow longer linux computer names than 15 chars

--- internal/spec/spec.go
+++ internal/spec/spec.go
@@ -635,6 +635,9 @@ func (r RunnerSpec) GetNewVMProperties(networkInterfaceID string, sizeSpec VMSiz
 	}
 
 	if r.BootstrapParams.OSType == params.Linux {
+		// Linux computer names can be up to 63 characters long.
+		properties.OSProfile.ComputerName = to.Ptr(r.BootstrapParams.Name)
+
 		pubKeys := []*armcompute.SSHPublicKey{}
 		fakeKey, err := providerUtil.GenerateFakeKey()
 		if err == nil {
